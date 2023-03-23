973c062d876f15e92df9d97aa339aab1e5af7444
SamirElkettani <samiric.78@gmail.com>
2023-03-23 16:14:53 -0500

    Done

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html", name="Samir", lastname="Elkettani")
+
+@app.route('/calculate/<int:x>/<int:y>')
+def calculator(x,y):
+    return render_template("body.html", x=x, y=y, sum = x + y)
+ 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000, debug=False)
